48218bc091acbcb3909ac4aabe2a4bfe3ec974c0
Professionalser2578 <globalmanpower.gms@gmail.com>
2024-06-27 09:46:32 +0530

    Update 03_number_guessing_game.cpp

--- 03_number_guessing_game.cpp
+++ 03_number_guessing_game.cpp
@@ -6,35 +6,33 @@ using namespace std;
 
 int main() 
 {
-        int n,random_number;  
-      
-        srand(time(0));
-      
-        n = 100;
-        random_number = rand() % n;
-      
-        int guess;
-        
-        while( guess != random_number )
+    int n, random_number;  
+
+    srand(time(0));
+
+    n = 100;
+    random_number = rand() % n;
+
+    int guess = -1;  // Initialize guess to a value that is not equal to random_number
+
+    while (guess != random_number)
+    {
+        cout << "Enter your guess: ";
+        cin >> guess;
+
+        if (guess == random_number)
         {
-                  cout << "Enter your guess: ";
-                  cin >> user_guess;
-              
-                  if( guess == random_number )
-                  {
-                      cout << "Congratulations! You guessed the right number!" << endl;
-                  }
-              
-                  else if( guess < random_number )
-                  {
-                      cout << "Your guess is too low. Enter a larger number" << endl;
-                  }
-              
-                  else
-                  {
-                      cout << "Your guess is too high. Enter a smaller number" << endl;
-                  }
+            cout << "Congratulations! You guessed the right number!" << endl;
         }
-      
-        return 0;
+        else if (guess < random_number)
+        {
+            cout << "Your guess is too low. Enter a larger number." << endl;
+        }
+        else
+        {
+            cout << "Your guess is too high. Enter a smaller number." << endl;
+        }
+    }
+
+    return 0;
 }
